663894900a81b5691db617874e37e2efd5a7eb30
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-12-16 15:30:55 -0800

    Basic Map View set up

--- MyForage/MyForage/ViewControllers/MapVC.swift
+++ MyForage/MyForage/ViewControllers/MapVC.swift
@@ -6,26 +6,62 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController, Storyboarded {
     
+    // TODO:
+    // array of locations
+    // custom annotation
+    // didSelect
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    // MARK: - Properties
+    
     weak var coordinator: MainCoordinator?
+    fileprivate let locationManager = CLLocationManager()
+    var span = MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15)
+    var userLocation: CLLocationCoordinate2D?
 
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        setUpMap()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Functions
+    
+    private func setUpMap() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
     }
-    */
 
+}
+
+extension MapVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let currentLocation = location.coordinate
+        userLocation = currentLocation
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation, span: span)
+        mapView.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
 }
