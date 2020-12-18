9909e15ba1ce54d6942a3419b6cf9cf8f9aa66c3
Zachary Thacker <zachthac@aol.com>
2020-12-17 19:15:59 -0500

    added data stack and convenience files for forage spot and notes

---
+++ MyForage/MyForage/Models/Note+Convenience.swift
@@ -0,0 +1,23 @@
+//
+//  Note+Convenience.swift
+//  MyForage
+//
+//  Created by Zachary Thacker on 12/17/20.
+//
+
+import Foundation
+import CoreData
+
+extension Note {
+    convenience init(body: String,
+                     date: Date,
+                     identifier: String,
+                     photo: String,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.body = body
+        self.date = date
+        self.identifier = identifier
+        self.photo = photo
+    }
+}
