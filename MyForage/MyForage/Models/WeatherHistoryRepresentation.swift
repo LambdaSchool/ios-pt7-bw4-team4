08b60eebb703054cd4d1005dcdf9953d75602894
Zachary Thacker <zachthac@aol.com>
2021-01-06 16:10:30 -0500

    finished the decoder and changed the core data models

---
+++ MyForage/MyForage/Models/WeatherHistoryRepresentation.swift
@@ -0,0 +1,68 @@
+//
+//  WeatherHistoryRepresentation.swift
+//  MyForage
+//
+//  Created by Zachary Thacker on 1/5/21.
+//
+
+import Foundation
+
+struct WeatherHistoryResults: Decodable {
+    let results: [WeatherHistoryRepresentation]
+}
+
+struct WeatherHistoryRepresentation: Decodable {
+    let dateTime: Date
+    let temperatureHigh: Double
+    let totalRain: Double
+    
+    enum Keys: String, CodingKey {
+        case hourly
+        case current
+        
+        enum CurrentKeys: String, CodingKey {
+            case dateTime = "dt"
+        }
+        enum HourlyKeys: String, CodingKey {
+            case temp
+            case rain
+            case rainMm = "1h"
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let rootDict = try decoder.container(keyedBy: Keys.self)
+        
+        let curretnContainer = try rootDict.nestedContainer(keyedBy: Keys.CurrentKeys.self, forKey: .current )
+        
+        dateTime = try curretnContainer.decode(Date.self, forKey: .dateTime)
+        var hourlyContainer = try rootDict.nestedUnkeyedContainer(forKey: .hourly)
+        
+        var highestTemp: Double = -100
+        var totalRainMm: Double = 0.0
+        
+        while hourlyContainer.isAtEnd == false {
+            
+            let hourContainer = try hourlyContainer.nestedContainer(keyedBy: Keys.HourlyKeys.self)
+            let hourlyTemp = try hourContainer.decode(Double.self, forKey: .temp)
+            if hourlyTemp > highestTemp {
+                highestTemp = hourlyTemp
+            }
+            
+            if let rainContainer = try? hourContainer.nestedContainer(keyedBy: Keys.HourlyKeys.self, forKey: .rain) {
+                let hourlyRain = try rainContainer.decode(Double.self, forKey: .rainMm)
+                totalRainMm += hourlyRain
+            }
+            
+        }
+        
+        temperatureHigh = highestTemp
+        totalRain = totalRainMm
+        
+        
+    }
+    
+    
+}
+
+
